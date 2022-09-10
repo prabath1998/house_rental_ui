7379df332cd1a9c01be798b4e27a717dc4d09cd7
prabath1998 <prabhathudayanga1998@gmail.com>
2022-09-10 13:12:07 +0530

    property list component added

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  property:any = {
-    "id":1,
-    "type" : "House",
-    "name" : "Birla house",
-    "price" : 12000
-  }
+  @Input() property : any;
+
+  // property:any = {
+  //   "id":1,
+  //   "type" : "House",
+  //   "name" : "Birla house",
+  //   "price" : 12000
+  // }
 
   constructor() { }
 
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -3,13 +3,68 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+  properties: Array<any> = [
+    {
+      id: 1,
+      type: 'Apartment',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 2,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 3,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 4,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 5,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 6,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 7,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 8,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+    {
+      id: 9,
+      type: 'House',
+      name: 'Birla house',
+      price: 12000,
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
